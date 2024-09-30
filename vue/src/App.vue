ad54ebf14f06cebff8f1a6f91ad92f9c78af0142
mouctar <mouctar.barry@outlook.com>
2024-09-30 16:13:23 +0200

    adding vue project

---
+++ vue/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="@/assets/build-a-bot-logo.png" alt="logo"/>
+          Build a bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+ <main>
+    <RobotBuilder />
+
+ </main>
+</template>
+
+<script>
+import RobotBuilder from '@/components/build/RobotBuilder.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RobotBuilder,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body{
+  background: linear-gradient(to bottom, #555, #999) fixed;
+}
+main{
+  margin: 0 auto;
+  padding: 30px;
+  background-color: azure;
+  min-height: 300px;
+  width: 1400px;
+}
+
+.robot{
+  height: 500px;
+}
+
+.home{
+  text-align: center;
+}
+
+header{
+  background-color: #999;
+  width: 1460px;
+  margin: 0 auto;
+}
+
+nav ul{
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item{
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.logo{
+  vertical-align: middle;
+  height: 30px;
+}
+
+</style>
